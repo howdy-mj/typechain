8af11fa8e6ccc79e00b1e63863a1fc8d3d89cf3f
mj <hi.minjungkim@gmail.com>
2020-05-24 16:22:30 +0900

    setting Typescript

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "MJ";
+const age = 25;
+const gender = "female";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and ${gender}`);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "MJ";
+const age = 25;
+const gender = "female";
+
+const sayHi = (name, age, gender) => {
+  console.log(`Hello ${name}, you are ${age} and ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
